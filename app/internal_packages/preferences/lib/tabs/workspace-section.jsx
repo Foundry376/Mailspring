8d3aebf7c84617b8eb141364c33e49f8b00953c1
Adrian Mester <ilogik@users.noreply.github.com>
2017-10-06 17:01:51 +0300

    Update workspace-section.jsx

--- app/internal_packages/preferences/lib/tabs/workspace-section.jsx
+++ app/internal_packages/preferences/lib/tabs/workspace-section.jsx
@@ -144,7 +144,7 @@ const WorkspaceSection = props => {
 
       <div className="platform-note platform-linux-only">
         &quot;Launch on system start&quot; only works in XDG-compliant desktop environments. To
-        enable the N1 icon in the system tray, you may need to install libappindicator1. (i.e.,
+        enable the Mailspring icon in the system tray, you may need to install libappindicator1. (i.e.,
         <code>sudo apt-get install libappindicator1</code>)
       </div>
     </section>
