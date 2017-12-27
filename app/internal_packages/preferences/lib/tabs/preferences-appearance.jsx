f360568e3f363bb2b9bf101f84feaef07826ee3a
Ben Gotow <ben@foundry376.com>
2017-12-27 10:34:40 -0500

    Expose option to change interface zoom #176

--- app/internal_packages/preferences/lib/tabs/preferences-appearance.jsx
+++ app/internal_packages/preferences/lib/tabs/preferences-appearance.jsx
@@ -2,10 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { RetinaImg, Flexbox } from 'mailspring-component-kit';
 
+class AppearanceScaleSlider extends React.Component {
+  static displayName = 'AppearanceScaleSlider';
+
+  static propTypes = {
+    id: PropTypes.string,
+    config: PropTypes.object.isRequired,
+  };
+
+  constructor(props) {
+    super();
+    this.kp = `core.workspace.interfaceZoom`;
+    this.state = { value: props.config.get(this.kp) };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ value: nextProps.config.get(this.kp) });
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} className="appearance-scale-slider">
+        <div className="ruler">
+          <div style={{ flex: 1.02 }}>
+            <RetinaImg name="appearance-scale-small.png" mode={RetinaImg.Mode.ContentDark} />
+          </div>
+          <div className="midpoint" />
+          <div style={{ flex: 2, textAlign: 'right' }}>
+            <RetinaImg name="appearance-scale-big.png" mode={RetinaImg.Mode.ContentDark} />
+          </div>
+        </div>
+        <input
+          type="range"
+          min={0.8}
+          max={1.4}
+          step={0.05}
+          value={this.state.value}
+          onChange={e => this.props.config.set(this.kp, e.target.value)}
+        />
+      </div>
+    );
+  }
+}
+
 class AppearanceModeSwitch extends React.Component {
   static displayName = 'AppearanceModeSwitch';
 
   static propTypes = {
+    id: PropTypes.string,
     config: PropTypes.object.isRequired,
   };
 
@@ -43,7 +87,7 @@ class AppearanceModeSwitch extends React.Component {
     if (!hasChanges) applyChangesClass += ' btn-disabled';
 
     return (
-      <div className="appearance-mode-switch">
+      <div id={this.props.id} className="appearance-mode-switch">
         <Flexbox direction="row" style={{ alignItems: 'center' }} className="item">
           {this._renderModeOptions()}
         </Flexbox>
@@ -92,11 +136,21 @@ class PreferencesAppearance extends React.Component {
   render() {
     return (
       <div className="container-appearance">
-        <label htmlFor="change-layout">Change layout:</label>
+        <h6 htmlFor="change-layout">Layout</h6>
         <AppearanceModeSwitch id="change-layout" config={this.props.config} />
-        <button className="btn btn-large" onClick={this.onClick}>
-          Change theme...
-        </button>
+        <h6 htmlFor="change-layout">Theme and Style</h6>
+        <div>
+          <button className="btn btn-large" onClick={this.onClick}>
+            Change theme...
+          </button>
+        </div>
+        <h6 htmlFor="change-scale">Scaling</h6>
+        <AppearanceScaleSlider id="change-scale" config={this.props.config} />
+        <div className="platform-note">
+          Scaling adjusts the entire UI, including icons, dividers, and text. Messages you send will
+          still have the same font size. Decreasing scale significantly may make dividers and icons
+          too small to click.
+        </div>
       </div>
     );
   }
