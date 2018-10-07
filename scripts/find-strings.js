b38afe5e3f65bafee5df72b307be1e61e15573f9
Ben Gotow <ben@foundry376.com>
2018-10-07 01:24:55 -0700

    Make the find-strings script prune localizations and give an overview of completeness

--- scripts/find-strings.js
+++ scripts/find-strings.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 let files = [];
 
+const PATH_TO_LANG = __dirname + '/../app/lang';
+const PATH_TO_ENGLISH = `${PATH_TO_LANG}/en.json`;
+
 function collectFiles(dir) {
   fs.readdirSync(dir).forEach(file => {
     const p = path.join(dir, file);
@@ -14,21 +17,21 @@ function collectFiles(dir) {
   });
 }
 
-function writeTerms(keys) {
+function writeTerms(terms, destPath) {
   const ordered = {};
-  Object.keys(keys)
+  Object.keys(terms)
     .sort()
     .forEach(function(key) {
-      ordered[key] = keys[key];
+      ordered[key] = terms[key];
     });
-  fs.writeFileSync(__dirname + '/../app/lang/en.json', JSON.stringify(ordered, null, 2));
+  fs.writeFileSync(destPath, JSON.stringify(ordered, null, 2));
 }
 
 collectFiles(__dirname + '/../app/src');
 collectFiles(__dirname + '/../app/internal_packages');
 collectFiles(__dirname + '/../app/menus');
 
-let keys = [];
+let sourceTerms = [];
 let found = 0;
 
 const start = /localized(?:ReactFragment)?\((['`"]{1})/g;
@@ -46,10 +49,38 @@ files.forEach(file => {
     const base = js.substr(startIndex, endIndex - startIndex);
 
     found += 1;
-    keys[base] = base;
+    sourceTerms[base] = base;
   }
 });
 
-console.log(`Found strings: ${found} localized() calls, ${Object.keys(keys).length} strings`);
+console.log('\nUpdating en.json to match strings in source:');
+console.log(`- ${found} localized() calls, ${Object.keys(sourceTerms).length} strings`);
+writeTerms(sourceTerms, PATH_TO_ENGLISH);
 
-writeTerms(keys);
+// Open other localization files, remove unneeded entries
+// and summarize localization progress
+
+console.log('\nPruning localized strings files:');
+console.log('\nLang\t\tStrings\t\t\tPercent');
+console.log('------------------------------------------------');
+fs.readdirSync(PATH_TO_LANG).forEach(filename => {
+  if (!filename.endsWith('.json')) return;
+  const localePath = path.join(PATH_TO_LANG, filename);
+  const localized = JSON.parse(fs.readFileSync(localePath).toString());
+
+  const inuse = {};
+  Object.keys(localized).forEach(term => {
+    if (sourceTerms[term]) {
+      inuse[term] = localized[term];
+    }
+  });
+  writeTerms(inuse, localePath);
+
+  const c = Object.keys(inuse).length;
+  const t = Object.keys(sourceTerms).length;
+  const lang = path.basename(filename, '.json');
+
+  console.log(
+    `- ${lang}\t${lang.length < 6 ? '\t' : ''}${c}\t/ ${t}\t\t${Math.round(c / t * 100)}%`
+  );
+});
