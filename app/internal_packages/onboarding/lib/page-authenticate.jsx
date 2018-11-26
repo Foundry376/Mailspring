ffef68198c3cf95c16727529123a30fb82309d08
quanzs <quanzhansheng@gmail.com>
2018-11-26 10:09:36 +0800

    skip login

--- app/internal_packages/onboarding/lib/page-authenticate.jsx
+++ app/internal_packages/onboarding/lib/page-authenticate.jsx
@@ -10,10 +10,11 @@ export default class AuthenticatePage extends React.Component {
   };
 
   _src() {
-    const n1Version = AppEnv.getVersion();
-    return `${MailspringAPIRequest.rootURLForServer(
-      'identity'
-    )}/onboarding?utm_medium=N1&utm_source=OnboardingPage&N1_version=${n1Version}&client_edition=basic`;
+    // const n1Version = AppEnv.getVersion();
+    // return `${MailspringAPIRequest.rootURLForServer(
+    //   'identity'
+    // )}/onboarding?utm_medium=N1&utm_source=OnboardingPage&N1_version=${n1Version}&client_edition=basic`;
+    return 'https://www.edison.tech/';
   }
 
   _onDidFinishLoad = webview => {
@@ -35,10 +36,33 @@ export default class AuthenticatePage extends React.Component {
     webview.executeJavaScript(openExternalLink);
   };
 
+  componentWillMount() {
+    this._skipLogin();
+  }
+
+  _skipLogin() {
+    const d = new Date();
+    const sysdata = d.getTime();
+    const result = {
+      "id": "id_" + sysdata,
+      "token": "token" + sysdata,
+      "firstName": "",
+      "lastName": "",
+      "emailAddress": "",
+      "object": "identity",
+      "createdAt": d.toISOString(),
+      "stripePlan": "Basic",
+      "stripePlanEffective": "Basic",
+      "stripeCustomerId": "cus_" + sysdata,
+      "stripePeriodEnd": "2099-12-31T00:00:00.000Z"
+    }
+    OnboardingActions.identityJSONReceived(result);
+  }
+
   render() {
     return (
       <div className="page authenticate">
-        <Webview src={this._src()} onDidFinishLoad={this._onDidFinishLoad} edisonAutoLogin={true}/>
+        {/* <Webview src={this._src()} onDidFinishLoad={this._onDidFinishLoad} edisonAutoLogin={true} /> */}
       </div>
     );
   }
