bf4805e73a4d5693b91b2d3910f1eac3efb543a6
quanzs <quanzhansheng@gmail.com>
2018-12-18 17:46:07 +0800

    chat: img preview

--- app/internal_packages/edison-beijing-chat/chat-components/components/chat/messages/MessageImagePopup.jsx
+++ app/internal_packages/edison-beijing-chat/chat-components/components/chat/messages/MessageImagePopup.jsx
@@ -17,7 +17,7 @@ export default class MessageImagePopup extends Component {
   }
   update() {
     console.log('MessageImagePopup update');
-    this.setState(Object.assign({}, this.state, {key:key++}));
+    this.setState(Object.assign({}, this.state, { key: key++ }));
   }
   getContactNameByJid(jid) {
     this.props = messageModel.messagesReactInstance.props;
@@ -110,7 +110,7 @@ export default class MessageImagePopup extends Component {
     let msg = messageModel.msg
     let index = group.messages.indexOf(msg);
     while (1) {
-      while (index<group.messages.length-1) {
+      while (index < group.messages.length - 1) {
         index++;
         msg = group.messages[index];
         let msgBody = isJsonString(msg.body) ? JSON.parse(msg.body) : msg.body;
@@ -122,7 +122,7 @@ export default class MessageImagePopup extends Component {
           return;
         }
       }
-      if (groupIndex < groupedMessages.length-1) {
+      if (groupIndex < groupedMessages.length - 1) {
         groupIndex++;
         group = groupedMessages[groupIndex];
         index = group.messages.length;
@@ -144,35 +144,34 @@ export default class MessageImagePopup extends Component {
     let msg = messageModel.msg;
     let msgBody = messageModel.msgBody;
 
-    return (<div className='message-image-popup' style={{top:'40px', left:'40px', width:(screen.width-80)+'px', height:(screen.height-160)+'px'}}>
-      <div className='topbar'>
-        <span id='close-button'onClick={()=>{this.hidden = true; this.update();}}></span>
-      </div>
-      <div className='message-image-popup-toolbar'>
-        {true || msg.sender !== messageModel.currentUserId ?
-          <div className="messageSender image-popup-avatar" style={{display:'inline-block'}}>
-            {messageModel.messagesReactInstance.getContactInfoByJid(msg.sender)}
-          </div> : null
-        }
-        <div style={{display:'inline-block'}}>
-          <span style={{display:'inline-block', width:'100%'}}>{msgBody.mediaObjectId.replace(/\.encrypted$/, '')}</span>
-          <span>{this.getContactNameByJid(msg.sender)} &nbsp; {timeDescriptor(msg.sentTime, true)}</span>
+    return (
+      <div className='message-image-popup'>
+        <div className='message-image-popup-toolbar'>
+          <span id='close-button' onClick={() => { this.hidden = true; this.update(); }}></span>
+          {true || msg.sender !== messageModel.currentUserId ?
+            <div className="messageSender image-popup-avatar" style={{ display: 'inline-block' }}>
+              {messageModel.messagesReactInstance.getContactInfoByJid(msg.sender)}
+            </div> : null
+          }
+          <div style={{ display: 'inline-block' }}>
+            <span style={{ display: 'inline-block', width: '100%' }}>{msgBody.mediaObjectId.replace(/\.encrypted$/, '')}</span>
+            <span>{this.getContactNameByJid(msg.sender)} &nbsp; {timeDescriptor(msg.sentTime, true)}</span>
+          </div>
+          <span className="download-img" style={{ float: 'right' }} onClick={this.downloadImage}></span>
         </div>
-        <span className="download-img" style={{float:'right'}} onClick={this.downloadImage}></span>
-      </div>
-        <div>
+        <div className='message-image-popup-content'>
           <div className="left-span">
-          <span className='left-arrow-button' onClick={this.gotoPrevImage}></span>
+            <span className='left-arrow-button' onClick={this.gotoPrevImage}></span>
           </div>
-          <img
-            src={msgBody.path}
-            style={{ width:(screen.width-180)+'px', height:(screen.height-160)+'px' }}
-          />
+          <div className="img-container">
+            <img src={msgBody.path} />
+          </div>
           <div className="right-span">
-          <span className='right-arrow-button' onClick={this.gotoNextImage}></span>
+            <span className='right-arrow-button' onClick={this.gotoNextImage}></span>
           </div>
         </div>
-      </div>)
+      </div>
+    )
   }
 }
 
