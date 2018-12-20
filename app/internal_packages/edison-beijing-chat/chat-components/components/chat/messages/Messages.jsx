e3b723ca54d520719ee8b2df31c16a5da260893e
Xingming Cao <xingming@edison.tech>
2018-12-20 09:35:41 +0800

    show message-toolbar on text message
    remove msg.showToolbar &&,  do not prevent css working

--- app/internal_packages/edison-beijing-chat/chat-components/components/chat/messages/Messages.jsx
+++ app/internal_packages/edison-beijing-chat/chat-components/components/chat/messages/Messages.jsx
@@ -370,7 +370,7 @@ export default class Messages extends PureComponent {
                       {timeDescriptor(msg.sentTime, true)}
                     </div>
                   </div>
-                  {msg.showToolbar && !msgFile && msg.sender === currentUserId && <div className='message-toolbar' >
+                  {!msgFile && msg.sender === currentUserId && <div className='message-toolbar' >
                     <span
                       className="inplace-edit-img"
                       onClick={startEditMessage}
