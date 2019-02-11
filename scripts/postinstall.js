4cb1851b4748f09c87f1d9ae94aabc3c8a9fa446
Ben Gotow <ben@foundry376.com>
2019-02-11 08:51:17 -0800

    Fix regression on some linux distros - need to set curl CURLOPT_CAINFO

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -15,7 +15,7 @@ const npmEnvs = {
     npm_config_target: npmElectronTarget,
     npm_config_arch: process.arch,
     npm_config_target_arch: process.arch,
-    npm_config_disturl: 'https://atom.io/download/atom-shell',
+    npm_config_disturl: 'https://atom.io/download/electron',
     npm_config_runtime: 'electron',
     npm_config_build_from_source: true,
   }),
