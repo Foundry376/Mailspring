4351084fe8f12838e0dcd466b8f5c2071aef049e
Ben Gotow <ben@foundry376.com>
2019-03-24 23:35:41 -0700

    Threshold diagonal swipe movements, reduce accidental swipe-to-archive #690

--- app/src/components/swipe-container.tsx
+++ app/src/components/swipe-container.tsx
@@ -140,11 +140,16 @@ export default class SwipeContainer extends React.Component<
     return this.isEnabled;
   };
 
-  _onWheel = e => {
+  _onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+    // Require that the wheel direction has a greater X component than Y component
+    const ratio = Math.abs(e.deltaX) / (Math.abs(e.deltaY) || 1);
+    if (this.phase !== Phase.GestureConfirmed && ratio < 1.3) return;
+
     let velocity = e.deltaX / 3;
     if (SwipeInverted) {
       velocity = -velocity;
     }
+
     this._onDragWithVelocity(velocity);
 
     if (this.phase === Phase.GestureConfirmed) {
