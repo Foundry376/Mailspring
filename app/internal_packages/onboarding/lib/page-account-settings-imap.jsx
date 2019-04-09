41eb4bce1f3ff2a385d8dd543838ea11b35201ee
quanzs <quanzhansheng@gmail.com>
2019-04-09 13:39:26 +0800

    DC-179

--- app/internal_packages/onboarding/lib/page-account-settings-imap.jsx
+++ app/internal_packages/onboarding/lib/page-account-settings-imap.jsx
@@ -171,6 +171,12 @@ class AccountIMAPSettingsForm extends React.Component {
     );
   }
 
+  componentWillMount() {
+    // auto fill username
+    this.props.account.settings['imap_username'] = this.props.account.emailAddress;
+    this.props.account.settings['smtp_username'] = this.props.account.emailAddress;
+  }
+
   renderFieldsForType(type) {
     return (
       <div>
