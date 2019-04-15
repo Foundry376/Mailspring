02d74b6a686f62640f49c37d0bfa90e2ffcfcaa7
quanzs <quanzhansheng@gmail.com>
2019-04-15 17:18:32 +0800

    DC-225

--- app/src/components/outline-view-item.jsx
+++ app/src/components/outline-view-item.jsx
@@ -2,7 +2,7 @@
 /* eslint jsx-a11y/tabindex-no-positive:0 */
 
 import _ from 'underscore';
-import { Utils } from 'mailspring-exports';
+import { Utils, AccountStore } from 'mailspring-exports';
 import classnames from 'classnames';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
@@ -319,6 +319,9 @@ class OutlineViewItem extends Component {
   }
 
   _renderItemContent(item = this.props.item, state = this.state) {
+    if (this.props.provider === 'aol' && item.name === 'Bulk Mail') {
+      item.displayName = item.name = 'Spam';
+    }
     if (state.editing) {
       const placeholder = item.inputPlaceholder || '';
       return (
@@ -368,10 +371,14 @@ class OutlineViewItem extends Component {
   }
 
   _renderChildren(item = this.props.item) {
+    let acc = {};
+    if (item.accountIds && item.accountIds.length === 1) {
+      acc = AccountStore.accountForId(item.accountIds[0]);
+    }
     if (item.children.length > 0 && !item.collapsed) {
       return (
         <section className="item-children" key={`${item.id}-children`}>
-          {item.children.map(child => <OutlineViewItem key={child.id} item={child} />)}
+          {item.children.map(child => <OutlineViewItem key={child.id} provider={acc.provider} item={child} />)}
         </section>
       );
     }
