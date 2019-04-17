533d574a2b35b00723f58db721e45b6316ed1fdc
quanzs <quanzhansheng@gmail.com>
2019-04-17 14:47:52 +0800

    update build windows installer scripts

--- app/build/create-signed-windows-installer.js
+++ app/build/create-signed-windows-installer.js
@@ -12,18 +12,18 @@ const { version } = require(path.join(appDir, 'package.json'));
 const config = {
   usePackageJson: false,
   outputDirectory: path.join(appDir, 'dist'),
-  appDirectory: path.join(appDir, 'dist', 'Edison Mail-win32-ia32'),
+  appDirectory: path.join(appDir, 'dist', 'Edison Mail-win32-x64'),
   loadingGif: path.join(appDir, 'build', 'resources', 'win', 'loading.gif'),
   iconUrl: 'http://mailspring-builds.s3.amazonaws.com/assets/mailspring.ico',
   certificateFile: process.env.WINDOWS_CODESIGN_CERT,
-  description: 'EdisonMail',
+  description: 'Edison Mail',
   version: version,
-  title: 'EdisonMail',
+  title: 'Edison Mail',
   authors: 'Foundry 376, LLC',
   setupIcon: path.join(appDir, 'build', 'resources', 'win', 'edisonmail.ico'),
   setupExe: 'EdisonMailSetup.exe',
   exe: 'Edison Mail.exe',
-  name: 'Edison Mail',
+  name: 'EdisonMail',
 };
 
 console.log(config);
@@ -36,6 +36,9 @@ createWindowsInstaller(config)
   .then(() => {
     console.log('createWindowsInstaller succeeded.');
     process.exit(0);
+  }, e => {
+    console.log(`createWindowsInstaller failed: ${e.message}`);
+    process.exit(1);
   })
   .catch(e => {
     console.error(`createWindowsInstaller failed: ${e.message}`);
