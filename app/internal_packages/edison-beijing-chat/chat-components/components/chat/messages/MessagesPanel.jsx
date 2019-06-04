b98fa5f6572e5bf48635a270cf27a25d3ac9064d
Xingming Cao <xingming@edison.tech>
2019-06-04 10:13:11 +0800

    tell the navigator online status while MessagePanel didMount

--- app/internal_packages/edison-beijing-chat/chat-components/components/chat/messages/MessagesPanel.jsx
+++ app/internal_packages/edison-beijing-chat/chat-components/components/chat/messages/MessagesPanel.jsx
@@ -144,6 +144,8 @@ export default class MessagesPanel extends PureComponent {
     this.getEmailContacts();
     window.addEventListener("online", this.onLine);
     window.addEventListener("offline", this.offLine);
+    const state = Object.assign({}, this.state, {online: navigator.onLine});
+    this.setState(state);
   }
   componentWillUnmount() {
     window.removeEventListener("online", this.onLine);
@@ -758,7 +760,7 @@ export default class MessagesPanel extends PureComponent {
                       style={{ width: 16 }}
                       isIcon
                       mode={RetinaImg.Mode.ContentIsMask} />
-                    <span>There appears to be a problem with your connection. Edison Mail is trying to reconnect. </span>
+                    <span>There appears to be a problem with your chat connection. Please click to reconnect: </span>
                     <span className="reconnect" onClick={this.reconnect}>Reconnect Now</span>
                   </div>
                 )
@@ -767,7 +769,7 @@ export default class MessagesPanel extends PureComponent {
                 style={{ width: 16 }}
                 isIcon
                 mode={RetinaImg.Mode.ContentIsMask} />
-              <span>Your computer appears to be offline. Check your network connection.</span>
+              <span>Your computer appears to be offline. Please check your network connection.</span>
             </div>)}
           </div>
         )}
