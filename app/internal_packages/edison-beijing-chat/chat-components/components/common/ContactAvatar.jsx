57551c64c5dcb259bcdf14ccb2bd7339dee7b6e6
quanzs <quanzhansheng@gmail.com>
2019-06-19 17:16:07 +0800

    Avatar  contact refactor

--- app/internal_packages/edison-beijing-chat/chat-components/components/common/ContactAvatar.jsx
+++ app/internal_packages/edison-beijing-chat/chat-components/components/common/ContactAvatar.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { gradientColorForString } from '../../../utils/colors';
-import { getAvatar, getAvatarFromCache, getProfile } from '../../../utils/restjs';
 import _ from 'underscore';
 import { getMyAppById } from '../../../utils/appmgt';
-import { ChatActions, OnlineUserStore } from 'chat-exports';
+import { ChatActions, OnlineUserStore, UserCacheStore } from 'chat-exports';
 
 const getInitials = name => {
   const trimmedName = name ? name.trim() : '';
@@ -22,7 +21,7 @@ const getInitials = name => {
 export default class ContactAvatar extends Component {
   constructor(props) {
     super(props);
-    this.refreshState(props);
+    this.state = {};
     this._listenToStore();
   }
 
@@ -50,14 +49,12 @@ export default class ContactAvatar extends Component {
     const bgColor = gradientColorForString(props.jid);
     let app;
     const state = {
-      avatar: props.avatar ? `https://s3.us-east-2.amazonaws.com/edison-profile-stag/${props.avatar}` : bgColor,
-      isImgExist: false,
-      userProfile: {},
+      avatar: UserCacheStore.getAvatarByJid(props.jid),
+      userInfo: UserCacheStore.getUserInfoByJid(props.jid),
       bgColor,
       isOnline: this.isOnline()
     };
 
-    const imgUrl = getAvatarFromCache(this.props.email);
     if (props.jid.match(/@app/)) {
       const conv = props.conversation;
       if (conv && conv.curJid) {
@@ -66,12 +63,8 @@ export default class ContactAvatar extends Component {
         app = getMyAppById(userId, id);
       }
     }
-    if (imgUrl) {
-      state.avatar = imgUrl;
-      state.isImgExist = true;
-    } else if (app) {
+    if (app) {
       state.avatar = app.icon;
-      state.isImgExist = true;
     }
     if (this.mounted) {
       this.setState(state);
@@ -87,40 +80,20 @@ export default class ContactAvatar extends Component {
 
   componentDidMount = async () => {
     this.mounted = true;
-    let { email, conversation } = this.props;
-    if (this.props.jid && !email && (!conversation || !conversation.isGroup)) {
-      let userProfile;
-      try {
-        const { data } = await getProfile(this.props.jid);
-        userProfile = data;
-      } catch (e) {
-        console.log('error in getProfile:', e);
-      }
-      if (userProfile) {
-        email = userProfile.email
-        this.setState({
-          userProfile: userProfile || {}
-        })
-      }
-    }
-    if (email && !this.props.avatar && !this.state.isImgExist) {
-      getAvatar(email, imgUrl => {
-        if (imgUrl) {
-          this.setState({
-            avatar: imgUrl,
-            isImgExist: true
-          })
-        }
-      })
-    }
     this.refreshState(this.props);
   }
 
+  _getContactName = () => {
+    const { name } = this.props;
+    const { userInfo } = this.state;
+    const contactName = name || (userInfo && userInfo.name) || (userInfo && userInfo.email);
+    return contactName;
+  }
+
   render() {
-    const { name, size = 48, conversation } = this.props;
-    const { avatar, isImgExist, userProfile, bgColor, isOnline } = this.state;
-    const isGroup = conversation && conversation.isGroup;
-    const contactName = name || userProfile.name;
+    const { size = 48 } = this.props;
+    const { avatar, bgColor, isOnline } = this.state;
+    const contactName = this._getContactName();
     const styles = {
       height: size,
       width: size,
@@ -130,9 +103,10 @@ export default class ContactAvatar extends Component {
       borderRadius: size / 2,
       background: bgColor
     }
-    if (isImgExist) {
-      styles.backgroundImage = `url(${avatar})`;
-      styles.backgroundSize = 'cover';
+    let avatarStyles = {};
+    if (avatar) {
+      avatarStyles.backgroundImage = `url(${avatar})`;
+      avatarStyles.backgroundSize = 'cover';
     }
     return (
       <div
@@ -140,7 +114,8 @@ export default class ContactAvatar extends Component {
         style={styles}
         title={contactName}
       >
-        {isImgExist ? null : getInitials(contactName).toUpperCase()}
+        <div className="avatar-image" style={avatarStyles}></div>
+        <span className="contact-name">{getInitials(contactName).toUpperCase()}</span>
         <div className={isOnline ? 'online' : 'offline'}></div>
       </div>
     )
--- app/internal_packages/edison-beijing-chat/store/UserCacheStore.es6
+++ app/internal_packages/edison-beijing-chat/store/UserCacheStore.es6
@@ -1,36 +1,95 @@
 import MailspringStore from 'mailspring-store';
 import UserCacheModel from '../model/UserCache';
+import fs from 'fs';
+import path from 'path';
+import _ from 'underscore';
+const download = require('download');
+let configDirPath = AppEnv.getConfigDirPath();
+let avatarDirPath = path.join(configDirPath, 'chat_avatar_cache');
 
+const AVATAR_BASE_URL = 'https://s3.us-east-2.amazonaws.com/edison-profile-stag/';
 class UserCacheStore extends MailspringStore {
   constructor() {
     super();
+    this.userCache = {};
+    this._triggerDebounced = _.debounce(() => this.trigger(), 20);
     this.loadUserCacheData();
   }
 
   loadUserCacheData = async () => {
-    this.userCache = {};
     const data = await UserCacheModel.findAll();
     for (const item of data) {
-      this.userCache[item.email] = item;
+      this.userCache[item.jid] = item;
     }
+    this._triggerDebounced();
   }
 
   refreshUserCache = async () => {
     await this.loadUserCacheData();
-    this.trigger();
   }
 
-  saveUserCache(members) {
+  saveUserCache = async (members) => {
     if (members) {
       for (const member of members) {
-        UserCacheModel.upsert({
+        await UserCacheModel.upsert({
+          jid: member.jid.bare,
           email: member.email,
           name: member.name,
           info: member
         });
       }
       this.refreshUserCache();
+      this.fetchAllAvatars(members);
     }
+  }
+
+  fetchAllAvatars = async (members) => {
+    if (members) {
+      for (const member of members) {
+        if (member.avatar) {
+          const avatarLocalPath = `${avatarDirPath}/${member.jid.bare}${path.extname(member.avatar)}`;
+          if (!fs.existsSync(avatarLocalPath)) {
+            try {
+              const data = await download(this._getAvatarUrl(member.avatar));
+              fs.writeFileSync(avatarLocalPath, data);
+              await UserCacheModel.update({
+                avatar: avatarLocalPath
+              }, {
+                  where: {
+                    jid: member.jid.bare
+                  }
+                });
+              await this.refreshUserCache();
+            } catch (e) {
+              console.warn('**avatar download error', member, e);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  getAvatarByJid = (jid) => {
+    const member = this.userCache[jid];
+    if (member && member.avatar) {
+      const avatarPath = member.avatar;
+      if (!fs.existsSync(avatarPath)) {
+        return encodeURI(avatarPath);
+      }
+      return this._getAvatarUrl(member.info.avatar);
+    }
+    return null;
+  }
+
+  getUserInfoByJid = (jid) => {
+    return this.userCache[jid];
+  }
+
+  _getAvatarUrl = (avatar) => {
+    if (avatar) {
+      return AVATAR_BASE_URL + avatar;
+    }
+    return null;
   }
 
   getUserCache = () => {
