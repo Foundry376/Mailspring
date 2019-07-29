976adbe98e6314f6657db8d6a9361991e60bde8c
quanzs <quanzhansheng@gmail.com>
2019-07-29 16:44:08 +0800

    chat: can display video player

--- app/internal_packages/edison-beijing-chat/components/chat/messages/MessagesPanel.jsx
+++ app/internal_packages/edison-beijing-chat/components/chat/messages/MessagesPanel.jsx
@@ -245,11 +245,11 @@ export default class MessagesPanel extends Component {
     const { msgBody, filepath } = loadConfig;
     log('load', `MessagePanel.loadMessage: type: ${loadConfig.type}, filepath: ${filepath}, mediaObjectId: ${msgBody.mediaObjectId}`);
     const loadCallback = (...args) => {
-      ChatActions.updateProgress({ loading: false, finished: true, visible: true });
+      ChatActions.updateProgress({ loading: false, finished: true, visible: true, mediaObjectId: msgBody.mediaObjectId });
       clearInterval(this.loadTimer);
       setTimeout(() => {
         if (!ProgressBarStore.progress.loading) {
-          ChatActions.updateProgress({ loading: false, finished: true, visible: false });
+          ChatActions.updateProgress({ loading: false, finished: true, visible: false, mediaObjectId: msgBody.mediaObjectId });
         }
       }, 3000);
       if (loadConfig.type === 'upload') {
@@ -300,7 +300,7 @@ export default class MessagesPanel extends Component {
       let jidLocal = conversation.jid.slice(0, atIndex);
       let aes = await MessageSend.getAESKey(conversation);
       loadConfig.aes = aes;
-      console.log( 'upload file: aes: ', aes);
+      console.log('upload file: aes: ', aes);
       try {
         loadConfig.request = uploadFile(
           jidLocal,
