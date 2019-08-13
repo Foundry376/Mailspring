09e25901f2e61008adab726e4fbc468992f40e1c
yangning <ning@edison.tech>
2019-08-13 15:58:11 +0800

    add richtext

---
+++ app/internal_packages/edison-beijing-chat/components/common/AtList.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ContactAvatar from './ContactAvatar';
+
+export default class AtList extends Component {
+  static propTypes = {
+    activeIndex: PropTypes.number,
+  };
+  constructor() {
+    super();
+    this.state = {};
+    this._scrollBox;
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        open: true,
+      });
+    }, 300);
+  }
+
+  componentWillReceiveProps({ activeIndex }) {
+    this.keepActiveRowInView(activeIndex);
+  }
+
+  keepActiveRowInView = activeIndex => {
+    if (this._scrollBox) {
+      const activeRow = this._scrollBox.querySelectorAll('.contact-row')[activeIndex];
+      if (activeRow) {
+        activeRow.scrollIntoView({ block: 'nearest' });
+      }
+    }
+  };
+
+  render() {
+    const { pos, contacts, activeIndex, chooseAtContact, changeAtActiveIndex } = this.props;
+
+    return (
+      <div
+        className="at-list-container"
+        style={{ bottom: pos.top + 33 + 'px', left: pos.left + 28 + 'px' }}
+        ref={el => (this._scrollBox = el)}
+        onMouseMove={event => {
+          // 监听相对于列表外面的盒子的鼠标移动事件，利用事件捕获，将事件转移到列表元素上
+          // 不直接监听列表元素是因为列表滚动也会在列表元素身上触发鼠标移动事件
+          const target = event.target;
+          if (target.className && target.className.indexOf('contact-row') >= 0) {
+            const nextActiveIndex = parseInt(target.offsetTop / target.offsetHeight);
+            changeAtActiveIndex(nextActiveIndex);
+          }
+        }}
+      >
+        {contacts.map((contact, index) => {
+          const active = index === activeIndex ? 'active' : '';
+          return (
+            <div
+              className={`contact-row ${active}`}
+              key={index}
+              onClick={() => chooseAtContact(contact)}
+            >
+              <ContactAvatar
+                jid={contact.jid}
+                name={contact.name}
+                email={contact.email}
+                size={16}
+              />
+              <span className="contact-name">{contact.name}</span>
+              <span className="contact-email">{contact.email}</span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
---
+++ app/internal_packages/edison-beijing-chat/components/common/RichText.jsx
@@ -0,0 +1,148 @@
+import React, { Component } from 'React';
+import PropTypes from 'prop-types';
+
+export class RichText extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    placeholder: PropTypes.string,
+    maxRows: PropTypes.number,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      listenKeys: new Set(),
+      listenKeysMapping: new Map(),
+    };
+    this._richText = null;
+  }
+
+  componentDidMount() {
+    const { keyMapping } = this.props;
+    const listenKeys = new Set();
+    const listenKeysMapping = new Map();
+    keyMapping.forEach(listenEvent => {
+      if (listenEvent && typeof listenEvent.keyCode === 'number') {
+        listenKeys.add(listenEvent.keyCode);
+        const theKeyEventList = listenKeysMapping.get(listenEvent.keyCode) || [];
+        theKeyEventList.push(listenEvent);
+        listenKeysMapping.set(listenEvent.keyCode, theKeyEventList);
+      }
+    });
+    this.setState({
+      listenKeys,
+      listenKeysMapping: listenKeysMapping,
+    });
+  }
+
+  _onKeyDown = event => {
+    const { listenKeys, listenKeysMapping } = this.state;
+    const { nativeEvent } = event;
+    const { keyCode } = nativeEvent;
+    if (!listenKeys.has(keyCode)) {
+      return true;
+    } else {
+      const keyEventList = listenKeysMapping.get(keyCode) || [];
+      for (const matchingKey of keyEventList) {
+        if (
+          !!matchingKey.altKey === !!event.altKey &&
+          !!matchingKey.ctrlKey === !!event.ctrlKey &&
+          !!matchingKey.shiftKey === !!event.shiftKey &&
+          !!matchingKey.metaKey === !!event.metaKey
+        ) {
+          if (matchingKey.preventDefault) {
+            event.preventDefault();
+          }
+          if (matchingKey.stopPropagation) {
+            event.stopPropagation();
+          }
+          if (matchingKey.keyEvent && typeof matchingKey.keyEvent === 'function') {
+            matchingKey.keyEvent(event);
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
+  _onInputChange = event => {
+    const { onChange } = this.props;
+    if (onChange && typeof onChange === 'function') {
+      onChange(event.target.innerHTML);
+    }
+  };
+
+  getNode = () => {
+    return this._richText.innerHTML;
+  };
+
+  addNode = content => {
+    if (!content) {
+      //如果插入的内容为空则返回
+      return;
+    }
+
+    let insertNode;
+    if (content instanceof HTMLElement) {
+      insertNode = content;
+      insertNode.setAttribute('contenteditable', false);
+    } else if (typeof content === 'string') {
+      insertNode = document.createTextNode(content);
+    }
+
+    this._richText.focus();
+
+    const sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+      let range = sel.getRangeAt(0);
+      range.deleteContents();
+      const frag = document.createDocumentFragment();
+      const lastNode = frag.appendChild(insertNode);
+      range.insertNode(frag);
+      const contentRange = range.cloneRange();
+      contentRange.setStartAfter(lastNode);
+      sel.removeAllRanges();
+      sel.addRange(contentRange);
+    }
+    const { onChange } = this.props;
+    if (onChange && typeof onChange === 'function') {
+      onChange(this.getNode());
+    }
+  };
+
+  clearNode = () => {
+    this._richText.innerHTML = '';
+  };
+
+  delNode = () => {
+    const sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+      let range = sel.getRangeAt(0);
+      range.deleteContents();
+      let contentRange = range.cloneRange();
+      sel.removeAllRanges();
+      sel.addRange(contentRange);
+    }
+  };
+
+  render() {
+    const { className, placeholder, maxRows = 3, lineHeight = 20, onBlur } = this.props;
+    return (
+      <div
+        className={className ? `rich-text-input ${className}` : 'rich-text-input'}
+        style={{
+          lineHeight: `${lineHeight}px`,
+          maxHeight: `${17 + lineHeight * maxRows}px`,
+        }}
+        dataplaceholder={placeholder}
+        contentEditable="true"
+        suppressContentEditableWarning="true"
+        onBlur={onBlur}
+        onKeyDown={this._onKeyDown}
+        onInput={this._onInputChange}
+        ref={el => (this._richText = el)}
+      />
+    );
+  }
+}
