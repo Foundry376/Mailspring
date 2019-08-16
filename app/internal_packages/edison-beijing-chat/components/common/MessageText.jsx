21d3fabcef9f524ee2c2c0f93a57512fa7991230
yangning <ning@edison.tech>
2019-08-16 11:18:38 +0800

    修改msg展示

--- app/internal_packages/edison-beijing-chat/components/common/MessageText.jsx
+++ app/internal_packages/edison-beijing-chat/components/common/MessageText.jsx
@@ -1,69 +1,34 @@
 /* eslint-disable prettier/prettier */
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { name } from '../../utils/name';
 
 const AT_BEGIN_CHAR = '\u0005';
 const AT_END_CHAR = '\u0004';
-const AT_INDEX_BASE = 0xf000;
 
-export default class MessageText extends Component {
-  static propTypes = {
-    text: PropTypes.string.isRequired,
-    atJids: PropTypes.array,
-  };
-  constructor() {
-    super();
-    this.state = { pieces: [] };
+function getName(jid) {
+  if (jid === 'all') {
+    return 'all';
+  } else {
+    return name(jid);
   }
-  getPieces = props => {
-    // to indicate the at name:
-    // send message will add \u0005 and \u0004 around the contact name
-    const { text } = props;
-    console.log('getPieces: ', text);
-    const pieces = [];
-    let pos = 0;
-    let i = 0;
-    // let jid;
-    while (i < text.length) {
-      if (text[i] === AT_BEGIN_CHAR) {
-        pieces.push({ text: text.substring(pos, i), type: 'normal' });
-        // const charCode = text.charCodeAt(i + 1);
-        // const jidIndex = charCode - AT_INDEX_BASE;
-        // jid = atJids[jidIndex];
-        pos = i + 1;
-      } else if (text[i] === AT_END_CHAR) {
-        const jid = text.substring(pos + 1, i); // @jid
-        console.log('getPieces: jid: ', jid);
-        let atName;
-        if (jid === 'all') {
-          atName = 'all';
-        } else {
-          atName = name(jid);
-        }
-        atName = '@' + (atName || text.substring(pos, i));
-        pieces.push({ text: atName, type: 'at' });
-        pos = i + 1;
-      }
-      i++;
+}
+
+export default function MessageText({ text }) {
+  // 创建匹配标记字符的正则，避免标记字符不是成对出现引起的问题
+  // .*? 非贪婪模式，最少匹配
+  const regStr = `(${AT_BEGIN_CHAR}@.*?${AT_END_CHAR})`;
+  const reg = new RegExp(regStr, 'g');
+  const newStr = text.replace(reg, `<span class="at">${'$1'}</span>`);
+  const dom = document.createElement('div');
+  dom.setAttribute('class', 'msg-text-with-at');
+  dom.innerHTML = newStr;
+  dom.childNodes.forEach(el => {
+    if (el.nodeType === 1) {
+      const nodeStr = el.innerHTML;
+      // del AT_BEGIN_CHAR、AT_END_CHAR and @
+      const jid = nodeStr.slice(2, -1);
+      el.innerHTML = getName(jid);
     }
-    if (pos < text.length) {
-      pieces.push({
-        text: text.substring(pos, text.length),
-        type: 'normal',
-      });
-    }
-    return pieces;
-  };
-  render() {
-    const pieces = this.getPieces(this.props);
-    const spans = pieces.map((piece, index) => {
-      return (
-        <span className={piece.type} key={index}>
-          {piece.text}
-        </span>
-      );
-    });
-    return <div className="msg-text-with-at">{spans}</div>;
-  }
+  });
+  return <div className="msg-text-with-at" dangerouslySetInnerHTML={{ __html: dom.innerHTML }} />;
 }
