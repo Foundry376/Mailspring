318a31d2791b34b2c7b27c473e1d9ef844a74dd0
Ben Gotow <ben@foundry376.com>
2019-10-10 20:50:06 -0500

    Leave vcard_grammar unpacked so mailsync can find it at runtime

--- app/build/tasks/package-task.js
+++ app/build/tasks/package-task.js
@@ -148,6 +148,7 @@ module.exports = grunt => {
             '**/src/tasks/**',
             '**/node_modules/spellchecker/**',
             '**/node_modules/windows-shortcuts/**',
+            '**/static/vcard_grammar',
           ].join(',') +
           '}',
       },
