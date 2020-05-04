39d8a21c0482906a5e009f2bba502d1b56ae2512
Ben Gotow <ben@foundry376.com>
2020-05-03 21:55:41 -0500

    Log sync exit codes and signals for easier triage on Windows

--- app/src/flux/models/account.ts
+++ app/src/flux/models/account.ts
@@ -1,6 +1,7 @@
 /* eslint global-require:0 */
 import * as Attributes from '../attributes';
 import { ModelWithMetadata } from './model-with-metadata';
+import { MailsyncProcessExit } from 'mailspring-exports';
 
 let CategoryStore = null;
 let Contact = null;
@@ -111,7 +112,7 @@ export class Account extends ModelWithMetadata {
   public aliases: string[];
   public defaultAlias: string;
   public syncState: string;
-  public syncError: string;
+  public syncError: MailsyncProcessExit | null;
   public color: string;
 
   constructor(args) {
