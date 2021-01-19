6bf9dda55b590606acc3c54bc78af2e2ab4ba88f
Janosch Maier <maierjanosch@gmail.com>
2021-01-19 23:24:57 +0100

    Add resize functionality for vertical thread list

--- app/internal_packages/thread-list/lib/thread-list-vertical.tsx
+++ app/internal_packages/thread-list/lib/thread-list-vertical.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
 import { InjectedComponentSet } from 'mailspring-component-kit';
 import { WorkspaceStore } from 'mailspring-exports';
+import { ResizableRegion, ResizableHandle } from '../../../src/components/resizable-region';
 
 class ThreadListVertical extends React.Component<{}, { style: string; syncing: boolean }> {
   static displayName = 'ThreadListVertical';
   static containerRequired = false;
 
   render() {
+    const minHeight = 150;
+    const initialHeight = AppEnv.getThreadListVerticalHeight() || 400;
     return (
       <>
-        <div style={{ height: '40%', width: '100%' }}>
+        <ResizableRegion
+          minHeight={minHeight}
+          initialHeight={initialHeight}
+          handle={ResizableHandle.Bottom}
+          onResize={h => this._onResize(h)}
+        >
           <InjectedComponentSet
             matching={{ location: WorkspaceStore.Location.ThreadList, modes: ['split'] }}
           />
-        </div>
-        <div style={{ height: '60%', width: '100%', borderTop: '0.5px solid #dddddd' }}>
-          <div className="sheet-toolbar" style={{ borderBottom: '0' }}>
+        </ResizableRegion>
+        <ResizableRegion
+          minHeight={minHeight}
+        >
+          <div style={{ height: '100%', width: '100%', borderTop: '0.5px solid #dddddd' }}>
+            <div className="sheet-toolbar" style={{ borderBottom: '0' }}>
+              <InjectedComponentSet
+                matching={{ location: WorkspaceStore.Location.MessageList.Toolbar, modes: ['split'] }}
+              />
+            </div>
             <InjectedComponentSet
-              matching={{ location: WorkspaceStore.Location.MessageList.Toolbar, modes: ['split'] }}
+              matching={{ location: WorkspaceStore.Location.MessageList, modes: ['split'] }}
             />
           </div>
-          <InjectedComponentSet
-            matching={{ location: WorkspaceStore.Location.MessageList, modes: ['split'] }}
-          />
-        </div>
+        </ResizableRegion>
       </>
     );
   }
+
+  _onResize = (height) => {
+    AppEnv.storeThreadListVerticalHeight(height);
+  };
+
 }
 
 export default ThreadListVertical;
