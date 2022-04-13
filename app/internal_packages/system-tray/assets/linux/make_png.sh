ca7fa8d77023c7ab02b897724debc61ff8360f16
Lorenzo Paulatto (naquite) <paulatz@gmail.com>
2022-04-13 18:28:06 +0200

    Nw set of icons light/dark tha fits quite well with kde/breeze theme

--- app/internal_packages/system-tray/assets/linux/make_png.sh
+++ app/internal_packages/system-tray/assets/linux/make_png.sh
@@ -1,8 +1,10 @@
- inkscape --export-dpi='270,270' --export-png=MenuItem-Inbox-Zero.png MenuItem-Inbox-Zero.svg
- inkscape --export-dpi='270,270' --export-png=MenuItem-Inbox-Full.png MenuItem-Inbox-Full.svg
- inkscape --export-dpi='270,270' --export-png=MenuItem-Inbox-Full-NewItems.png MenuItem-Inbox-Full-NewItems.svg
+res=$[96*4]
+#res=96
+ inkscape --export-dpi="$res,$res" --export-png=MenuItem-Inbox-Zero.png MenuItem-Inbox-Zero.svg
+ inkscape --export-dpi="$res,$res" --export-png=MenuItem-Inbox-Full.png MenuItem-Inbox-Full.svg
+ inkscape --export-dpi="$res,$res" --export-png=MenuItem-Inbox-Full-NewItems.png MenuItem-Inbox-Full-NewItems.svg
 
- inkscape --export-dpi='270,270' --export-png=MenuItem-Inbox-Zero-light.png MenuItem-Inbox-Zero-light.svg
- inkscape --export-dpi='270,270' --export-png=MenuItem-Inbox-Full-light.png MenuItem-Inbox-Full-light.svg
- inkscape --export-dpi='270,270' --export-png=MenuItem-Inbox-Full-NewItems-light.png MenuItem-Inbox-Full-NewItems-light.svg
+ inkscape --export-dpi="$res,$res" --export-png=MenuItem-Inbox-Zero-light.png MenuItem-Inbox-Zero-light.svg
+ inkscape --export-dpi="$res,$res" --export-png=MenuItem-Inbox-Full-light.png MenuItem-Inbox-Full-light.svg
+ inkscape --export-dpi="$res,$res" --export-png=MenuItem-Inbox-Full-NewItems-light.png MenuItem-Inbox-Full-NewItems-light.svg
 
