5616b829429fc74bbbe1e13c0ed7eca9c986ae0a
Janosch Braukmann <maierjanosch@gmail.com>
2023-10-09 21:20:19 +0200

    Migrate from Keytar to SafeStorage (#2471)
    * Switch credential storage from keytar to electrons key storage
    
    * feat: Migrate credentials from keytar
    
    * feat: add function to cleanup old keytar credentials
    
    * refactor: Remove CredentialStore
    
    * feat: Remove old keytar credentials on migration

--- app/src/key-manager.ts
+++ app/src/key-manager.ts
@@ -1,4 +1,3 @@
-
 import keytar from 'keytar';
 import { localized } from './intl';
 import { Account } from 'mailspring-exports';
@@ -7,8 +6,13 @@ interface KeySet {
   [key: string]: string;
 }
 
+const { safeStorage } = require("@electron/remote");
+
+const configCredentialsKey = 'credentials';
+const configCredentialsMigratedKey = 'credentialsMigratedFromKeytar';
+
 /**
- * A basic wrap around keytar's secure key management. Consolidates all of
+ * A basic wrap around electron's secure key management. Consolidates all of
  * our keys under a single namespaced keymap and provides migration
  * support.
  *
@@ -16,8 +20,31 @@ interface KeySet {
  * and every key we want to access.
  */
 class KeyManager {
-  SERVICE_NAME = AppEnv.inDevMode() ? 'Mailspring Dev' : 'Mailspring';
-  KEY_NAME = 'Mailspring Keys';
+
+  // TODO: Delete the migration from Keytar once its dependency can be removed
+  constructor() {
+    const SERVICE_NAME = AppEnv.inDevMode() ? 'Mailspring Dev' : 'Mailspring';
+    const KEY_NAME = 'Mailspring Keys';
+
+    if (!this.isMigrated()) {
+      console.log("Keys not yet migrated. Migrating now...");
+      keytar.getPassword(SERVICE_NAME, KEY_NAME).then(raw => {
+        const keys = JSON.parse(raw) as KeySet;
+        this._writeKeyHash(keys);
+        keytar.deletePassword(SERVICE_NAME, KEY_NAME);
+        this.setMigrated();
+      });
+    }
+  }
+
+  isMigrated() {
+    const result = AppEnv.config.get(configCredentialsMigratedKey) || false
+    return result;
+  }
+
+  setMigrated() {
+    AppEnv.config.set(configCredentialsMigratedKey, 'true');
+  }
 
   async deleteAccountSecrets(account: Account) {
     try {
@@ -68,13 +95,13 @@ class KeyManager {
   }
 
   async deletePassword(keyName: string) {
+    const keys = await this._getKeyHash();
     try {
-      const keys = await this._getKeyHash();
       delete keys[keyName];
-      await this._writeKeyHash(keys);
     } catch (err) {
-      this._reportFatalError(err);
+      console.log('Could not delete password as it did not exist', err);
     }
+    await this._writeKeyHash(keys);
   }
 
   async getPassword(keyName: string) {
@@ -87,7 +114,20 @@ class KeyManager {
   }
 
   async _getKeyHash() {
-    const raw = (await keytar.getPassword(this.SERVICE_NAME, this.KEY_NAME)) || '{}';
+    // TODO: Remove when Keytar is completely removed
+    // Wait until key migration has finished
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+    let isMigrated = this.isMigrated();
+    while (!isMigrated) {
+      await delay(1000);
+      isMigrated = this.isMigrated();
+    }
+
+    let raw = '{}';
+    const encryptedCredentials = AppEnv.config.get(configCredentialsKey);
+    if (encryptedCredentials !== undefined) {
+      raw = await safeStorage.decryptString(Buffer.from(encryptedCredentials, "utf-8"));
+    }
     try {
       return JSON.parse(raw) as KeySet;
     } catch (err) {
@@ -96,7 +136,8 @@ class KeyManager {
   }
 
   async _writeKeyHash(keys: KeySet) {
-    await keytar.setPassword(this.SERVICE_NAME, this.KEY_NAME, JSON.stringify(keys));
+    const enrcyptedCredentials = await safeStorage.encryptString(JSON.stringify(keys))
+    AppEnv.config.set(configCredentialsKey, enrcyptedCredentials);
   }
 
   _reportFatalError(err: Error) {
