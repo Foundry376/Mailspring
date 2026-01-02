18295c5d7376e73907941de5fafff32f431f03e1
Ben Gotow <ben@foundry376.com>
2026-01-01 22:47:46 -0600

    Fix issues

--- app/internal_packages/events/lib/event-header.tsx
+++ app/internal_packages/events/lib/event-header.tsx
@@ -46,7 +46,7 @@ we fall back to storing the RSVP status in message metadata (so the "Accept" but
 export class EventHeader extends React.Component<EventHeaderProps, EventHeaderState> {
   static displayName = 'EventHeader';
 
-  state = {
+  state: EventHeaderState = {
     icsEvent: undefined,
     icsMethod: undefined,
     icsOriginalData: undefined,
@@ -109,9 +109,9 @@ export class EventHeader extends React.Component<EventHeaderProps, EventHeaderSt
 
     // Workaround to convert calendar invites sent out from Microsoft calendars to IANA timezones
     // that can be handled by moments-timezone.
-    let startTimezone = findOneIana(icsEvent.startDate.timezone) || icsEvent.startDate.timezone;
-    let endTimezone = findOneIana(icsEvent.endDate.timezone) || icsEvent.endDate.timezone;
-
+    let startTimezone = findOneIana(icsEvent.startDate.zone.tzid) || icsEvent.startDate.zone.tzid;
+    let endTimezone = findOneIana(icsEvent.endDate.zone.tzid) || icsEvent.endDate.zone.tzid;
+    console.log(startTimezone, endTimezone, icsEvent, icsEvent.startDate.toString());
     // Workaround to convert calendar invites sent out from Google calendar with "Z" timezone
     // to IANA timezone that can be handled by moments-timezone.
     if (startTimezone === 'Z') {
@@ -196,7 +196,7 @@ export class EventHeader extends React.Component<EventHeaderProps, EventHeaderSt
 
     let status = me.status;
 
-    const icsTimeProperty = icsEvent.component.getFirstPropertyValue('dtstamp');
+    const icsTimeProperty = icsEvent.component.getFirstPropertyValue('dtstamp') as ICAL.Time;
     const icsTime = icsTimeProperty ? icsTimeProperty.toJSDate() : new Date(0);
 
     const metadata = this.props.message.metadataForPluginId('event-rsvp');
